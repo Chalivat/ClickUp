bdb8c11444b665996d1cf69190cad62867898b5b
Chalivat <quentin.chalivat@e-artsup.net>
2021-02-13 01:11:16 +0100

    jump and Aim v1

--- Assets/_Scripts/Jump.cs
+++ Assets/_Scripts/Jump.cs
@@ -1,18 +1,59 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Jump : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float jumpForce;
+    public float clickIntensity;
+    private bool isClicking;
+    public float chargingSpeed;
+
+    private Rigidbody rb;
+    public Transform GravityCenter;
+    public Transform Bumper;
+
+    public Image chargeBar;
+    void DoJump()
     {
-        
+        //rb.AddForce(transform.up * clickIntensity,ForceMode.VelocityChange);
+        rb.velocity = transform.up * clickIntensity * jumpForce;
     }
 
-    // Update is called once per frame
+    void ChargeJump()
+    {
+        if (Input.GetAxis("Jump") > .2f && clickIntensity <=1)
+        {
+            clickIntensity += Time.deltaTime * chargingSpeed;
+            clickIntensity = Mathf.Clamp01(clickIntensity);
+            UpdateBar(clickIntensity);
+            Bumper.localScale = new Vector3(1,Mathf.Lerp(1,0,clickIntensity),1);
+        }
+        else if (clickIntensity >=.1f)
+        {
+            DoJump();
+            clickIntensity = 0;
+
+            Bumper.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
     void Update()
     {
-        
+        ChargeJump();
+    }
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        Physics.gravity = Physics.gravity * 2;
+        rb.centerOfMass = GravityCenter.localPosition;
+    }
+
+    void UpdateBar(float value)
+    {
+        chargeBar.fillAmount = value;
     }
 }
---
+++ Assets/_Scripts/Viseur.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Viseur : MonoBehaviour
+{
+    public float AimingSpeed;
+
+    void Aim()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(x,y,0);
+        
+        //dir = newRot.eulerAngles;
+        print(dir);
+        if (dir.magnitude >= .1f)
+        {
+            transform.rotation = Quaternion.LookRotation(dir, -Vector3.forward) * Quaternion.FromToRotation(Vector3.up, Vector3.forward);
+        }
+    }
+
+    void Update()
+    {
+        Aim();
+    }
+}
